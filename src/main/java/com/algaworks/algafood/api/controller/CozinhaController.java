b989666144a9c440adac6847d1bc11bd0e2bedbf
rmls <rodrigomoreiralima@hotmail.com>
2023-02-09 11:15:00 -0300

    feat: criando consulta buscar by id

---
+++ src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -0,0 +1,27 @@
+package com.algaworks.algafood.api.controller;
+
+import com.algaworks.algafood.domain.model.Cozinha;
+import com.algaworks.algafood.domain.repository.CozinhaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/cozinhas")
+public class CozinhaController {
+    
+    @Autowired
+    private CozinhaRepository cozinhaRepository;
+    
+    @GetMapping
+    public List<Cozinha> listar() { return cozinhaRepository.todas(); }
+    
+    @GetMapping("/{cozinhaId}")
+    public Cozinha buscar(@PathVariable Long cozinhaId) {
+        return cozinhaRepository.porId(cozinhaId);
+    }
+}
--- src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
+++ src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
@@ -1,5 +1,6 @@
 package com.algaworks.algafood.domain.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -15,6 +16,7 @@ public class Cozinha {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     
+
     @Column(nullable = false)
     private String nome;
     
