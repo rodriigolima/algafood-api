da336fe2e646a44a3a4ffcbb49630510f739b205
rmls <rodrigomoreiralima@hotmail.com>
2023-02-23 21:21:18 -0300

    feat: criando um repository's customizados

--- src/main/java/com/algaworks/algafood/domain/repository/RestauranteRepository.java
+++ src/main/java/com/algaworks/algafood/domain/repository/RestauranteRepository.java
@@ -2,15 +2,29 @@ package com.algaworks.algafood.domain.repository;
 
 import com.algaworks.algafood.domain.model.Restaurante;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.math.BigDecimal;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
-public interface RestauranteRepository extends JpaRepository<Restaurante, Long> {
+public interface RestauranteRepository extends CustomJpaRepository<Restaurante, Long>, RestauranteRepositoryQueries, 
+        JpaSpecificationExecutor<Restaurante> {
     
     List<Restaurante> findByTaxaFreteBetween(BigDecimal taxaInicial, BigDecimal taxaFinal);
     
+//    @Query("from Restaurante where nome like %:nome% and cozinha.id = :id")
+    List<Restaurante> consultarPorNome(String nome, @Param("id") Long cozinhaId);
+    
     List<Restaurante> findByNomeContainingAndCozinhaId(String nome, Long cozinhaId);
+    
+    Optional<Restaurante> findFirstByNomeContaining(String nome);
+    
+    List<Restaurante> findTop2ByNomeContaining(String nome);
+    
+    int countByCozinhaId(Long cozinhaId);
+    
 }
