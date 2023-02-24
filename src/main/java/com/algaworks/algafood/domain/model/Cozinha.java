10b19e3760dad4b31b365db73b2700b70782f5cc
rmls <rodrigomoreiralima@hotmail.com>
2023-02-23 23:17:54 -0300

    feat: aplicando relacinamento entre restaurante e forma_pagamento + recurso de insert

--- src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
+++ src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
@@ -1,9 +1,12 @@
 package com.algaworks.algafood.domain.model;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
@@ -20,4 +23,7 @@ public class Cozinha {
     @Column(nullable = false)
     private String nome;
     
+    @JsonIgnore
+    @OneToMany(mappedBy = "cozinha")
+    private List<Restaurante> restaurantes = new ArrayList<>();
 }
