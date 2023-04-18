6a15d0aa8df60bc8841128da9b48c26b382f5334
rmls <rodrigomoreiralima@hotmail.com>
2023-04-18 20:29:39 -0300

    test: apresentando bons nomes nos testes

--- src/test/java/com/algaworks/algafood/CadastroCozinhaIT.java
+++ src/test/java/com/algaworks/algafood/CadastroCozinhaIT.java
@@ -1,5 +1,7 @@
 package com.algaworks.algafood;
 
+import com.algaworks.algafood.domain.exception.CozinhaNaoEncontradaException;
+import com.algaworks.algafood.domain.exception.EntidadeEmUsoException;
 import com.algaworks.algafood.domain.model.Cozinha;
 import com.algaworks.algafood.domain.service.CadastroCozinhaService;
 import org.junit.jupiter.api.Test;
@@ -13,27 +15,43 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 
 @SpringBootTest
-class CadastroCozinhaIntegrationTests {
-	
+class CadastroCozinhaIT {
+
 	@Autowired
 	private CadastroCozinhaService cadastroCozinhaService;
 
 	@Test
-	void testarCadastroCozinhaComSucesso() {
+	void deveAtribuir_QuandoCadastrarCozinhaComDadosCorreto() {
 		Cozinha novaCozinha = new Cozinha();
 		novaCozinha.setNome("Chinesa");
-		
+
 		novaCozinha = cadastroCozinhaService.salvar(novaCozinha);
-		
+
 		assertNotNull(novaCozinha);
 		assertNotNull(novaCozinha.getId());
 	}
 
 	@Test
-	void testarCadastroCozinhaSemNome() {
+	void deveFalhar_QuandoCadastrarCozinhaSemNome() {
 		Cozinha novaCozinha = new Cozinha();
 		novaCozinha.setNome(null);
+
+		assertThrows(ConstraintViolationException.class, ()-> cadastroCozinhaService.salvar(novaCozinha));
+	}
+
+	@Test
+	void deveFalhar_QuandoExcluirCozinhaEmUso() {
+		EntidadeEmUsoException erroEsperado = assertThrows(EntidadeEmUsoException.class,
+				() -> cadastroCozinhaService.excluir(1L));
+
+		assertNotNull(erroEsperado);
+	}
+
+	@Test
+	void deveFalhar_QuandoExcluirCozinhaInexistente() {
+		CozinhaNaoEncontradaException erroEsperado = assertThrows(CozinhaNaoEncontradaException.class,
+				() -> cadastroCozinhaService.excluir(100L));
 		
-		assertThrows(ConstraintViolationException.class, ()-> {cadastroCozinhaService.salvar(novaCozinha);});
+		assertNotNull(erroEsperado);
 	}
 }
