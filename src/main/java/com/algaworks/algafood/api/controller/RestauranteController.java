05e3fe6ec5abfb5db7532dfa56dc86ff8a768874
rmls <rodrigomoreiralima@hotmail.com>
2023-06-08 00:13:55 -0300

    fix: consertando problema com as capturas de exceptions

--- src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
+++ src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
@@ -12,24 +12,24 @@ import com.algaworks.algafood.domain.model.Restaurante;
 import com.algaworks.algafood.domain.repository.RestauranteRepository;
 import com.algaworks.algafood.domain.service.CadastroRestauranteService;
 import com.fasterxml.jackson.annotation.JsonView;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
-import javax.validation.Valid;
 import java.util.List;
 
 @RestController
 @RequestMapping("/restaurantes")
 public class RestauranteController {
-    
+
     @Autowired
     private RestauranteRepository restauranteRepository;
-    
+
     @Autowired
     private CadastroRestauranteService cadastroRestaurante;
-    
-    
+
+
     @Autowired
     private RestauranteModelAssembler restauranteModelAssembler;
 
@@ -39,7 +39,7 @@ public class RestauranteController {
     @JsonView(RestauranteView.Resumo.class)
     @GetMapping
     public List<RestauranteDTO> listar() {
-        return restauranteModelAssembler.toCollectionModel(restauranteRepository.findAll()); 
+        return restauranteModelAssembler.toCollectionModel(restauranteRepository.findAll());
     }
 
     @JsonView(RestauranteView.ApenasNome.class)
@@ -47,33 +47,33 @@ public class RestauranteController {
     public List<RestauranteDTO> listarApenasNome() {
         return listar();
     }
-    
-    
+
+
     @GetMapping("/{restauranteId}")
     public RestauranteDTO buscar(@PathVariable Long restauranteId) {
         Restaurante restaurante =  cadastroRestaurante.buscarOuFalhar(restauranteId);
 
         return restauranteModelAssembler.toModel(restaurante);
     }
-    
+
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public RestauranteDTO adicionar(@RequestBody @Valid RestauranteInput restauranteInput) {
         try {
             Restaurante restaurante = restauranteInputDisassembler.toDomainObject(restauranteInput);
-            
+
             return  restauranteModelAssembler.toModel(cadastroRestaurante.salvar(restaurante));
         } catch (EntidadeNaoEncontradaException e) {
             throw new NegocioException(e.getMessage(), e);
         }
     }
-    
+
     @PutMapping("/{restauranteId}")
-    public RestauranteDTO atualizar(@PathVariable Long restauranteId, 
+    public RestauranteDTO atualizar(@PathVariable Long restauranteId,
                                     @RequestBody @Valid RestauranteInput restauranteInput) {
         try {
             Restaurante restauranteAtual = cadastroRestaurante.buscarOuFalhar(restauranteId);
-            
+
             restauranteInputDisassembler.copyToDomainObject(restauranteInput, restauranteAtual);
 
             return restauranteModelAssembler.toModel(cadastroRestaurante.salvar(restauranteAtual));
@@ -81,7 +81,7 @@ public class RestauranteController {
             throw new NegocioException(e.getMessage(), e);
         }
     }
-    
+
     @PutMapping("/{restauranteId}/ativo")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void ativar(@PathVariable Long restauranteId) {
@@ -93,7 +93,7 @@ public class RestauranteController {
     public void inativar(@PathVariable Long restauranteId) {
         cadastroRestaurante.inativar(restauranteId);
     }
-    
+
     @PutMapping("/ativacoes")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void ativarMultiplos(@RequestBody List<Long> restauranteIds) {
@@ -113,7 +113,7 @@ public class RestauranteController {
             throw new NegocioException(ex.getMessage(), ex);
         }
     }
-    
+
     @PutMapping("/{restauranteId}/abertura")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void abrir(@PathVariable Long restauranteId) {
