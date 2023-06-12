10dad15de4c346ccb622f918243a7fbbb1e93450
rmls <rodrigomoreiralima@hotmail.com>
2023-06-11 21:31:14 -0300

    feat: Validando o content type do arquivo

--- src/main/java/com/algaworks/algafood/api/model/input/FotoProdutoInput.java
+++ src/main/java/com/algaworks/algafood/api/model/input/FotoProdutoInput.java
@@ -1,17 +1,20 @@
 package com.algaworks.algafood.api.model.input;
 
+import com.algaworks.algafood.core.validation.FileContentType;
 import com.algaworks.algafood.core.validation.FileSize;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.http.MediaType;
 import org.springframework.web.multipart.MultipartFile;
 
 @Getter@Setter
 public class FotoProdutoInput {
 
     @NotNull
-    @FileSize(max = "50KB")
+    @FileSize(max = "500KB")
+    @FileContentType(allowed = {MediaType.IMAGE_JPEG_VALUE, MediaType.IMAGE_PNG_VALUE})
     private MultipartFile arquivo;
 
     @NotBlank
---
+++ src/main/java/com/algaworks/algafood/core/validation/FileCotentTypeValidator.java
@@ -0,0 +1,23 @@
+package com.algaworks.algafood.core.validation;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class FileCotentTypeValidator implements ConstraintValidator<FileContentType, MultipartFile>  {
+
+    private List<String> allowedContentTypes;
+
+    @Override
+    public void initialize(FileContentType constraintAnnotation) {
+        this.allowedContentTypes = Arrays.asList(constraintAnnotation.allowed());
+    }
+
+    @Override
+    public boolean isValid(MultipartFile value, ConstraintValidatorContext context) {
+        return value == null || allowedContentTypes.contains(value.getContentType());
+    }
+}
