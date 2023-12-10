7a04b36f8128274a37392cb1ab33833d2953a2ab
rodriigolima <rodrigomoreiralima@hotmail.com>
2023-12-10 19:42:21 -0300

    feet: aplicaindo deep etag na forma de pagamento.

--- server/src/main/java/com/algaworks/algafood/api/controller/FormaPagamentoController.java
+++ server/src/main/java/com/algaworks/algafood/api/controller/FormaPagamentoController.java
@@ -9,10 +9,16 @@ import com.algaworks.algafood.domain.repository.FormaPagamentoRepository;
 import com.algaworks.algafood.domain.service.CadastroFormaPagamentoService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.CacheControl;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.context.request.ServletWebRequest;
+import org.springframework.web.filter.ShallowEtagHeaderFilter;
 
+import java.time.OffsetDateTime;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping("/formas-pagamento")
@@ -29,17 +35,57 @@ public class FormaPagamentoController {
     
     @Autowired
     private FormaPagamentoInputDisassembler formaPagamentoInputDisassembler;
-    
+
     @GetMapping
-    public List<FormaPagamentoDTO> listar() {
-        return formaPagamentoModelAssembler.toCollectionModel(formaPagamentoRepository.findAll());
+    public ResponseEntity<List<FormaPagamentoDTO>> listar(ServletWebRequest request) {
+        ShallowEtagHeaderFilter.disableContentCaching(request.getRequest());
+
+        String eTag = "0";
+
+        OffsetDateTime dataUltimaAtualizacao = formaPagamentoRepository.getDataUltimaAtualizacao();
+
+        if (dataUltimaAtualizacao != null)
+            eTag = String.valueOf(dataUltimaAtualizacao.toEpochSecond());
+
+        if (request.checkNotModified(eTag))
+            return null;
+
+        List<FormaPagamento> todasFormasPagamentos = formaPagamentoRepository.findAll();
+
+        List<FormaPagamentoDTO> formasPagamentosModel = formaPagamentoModelAssembler
+                .toCollectionModel(todasFormasPagamentos);
+
+        return ResponseEntity.ok()
+                .cacheControl(CacheControl.maxAge(10, TimeUnit.SECONDS).cachePublic())
+                .eTag(eTag)
+                .body(formasPagamentosModel);
     }
 
     @GetMapping("/{formaPagamentoId}")
-    public FormaPagamentoDTO buscar(@PathVariable Long formaPagamentoId) {
+    public ResponseEntity<FormaPagamentoDTO> buscar(@PathVariable Long formaPagamentoId, ServletWebRequest request) {
+        ShallowEtagHeaderFilter.disableContentCaching(request.getRequest());
+
+        String eTag = "0";
+
+        OffsetDateTime dataAtualizacao = formaPagamentoRepository
+                .getDataAtualizacaoById(formaPagamentoId);
+
+        if (dataAtualizacao != null) {
+            eTag = String.valueOf(dataAtualizacao.toEpochSecond());
+        }
+
+        if (request.checkNotModified(eTag)) {
+            return null;
+        }
+
         FormaPagamento formaPagamento = cadastroFormaPagamento.buscarOuFalhar(formaPagamentoId);
 
-        return formaPagamentoModelAssembler.toModel(formaPagamento);
+        FormaPagamentoDTO formaPagamentoModel = formaPagamentoModelAssembler.toModel(formaPagamento);
+
+        return ResponseEntity.ok()
+                .cacheControl(CacheControl.maxAge(10, TimeUnit.SECONDS))
+                .eTag(eTag)
+                .body(formaPagamentoModel);
     }
 
     @PostMapping
